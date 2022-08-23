f6f92b6697a138c4720a8fa318b9d42092193074
shekharbabu28 <105600968+shekharbabu28@users.noreply.github.com>
2022-08-23 12:00:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -18,9 +18,9 @@ def Testing(jobnames)
   sh "java -jar /root/.jenkins/workspace/${jobnames}/testing.jar"
 }
 
-def mail(body,subject,email_id)
+def mail(body,subject,email)
 { 
-  mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${email_id}"
+  mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${email}"
 }
 
 
